0041406ff707fbdf3223baa74f9740abc1e4d19a
Maria Heblik <maria@bsolut.com>
2015-06-05 16:50:21 +0200

    response isInt fix

--- jirabot.js
+++ jirabot.js
@@ -1,16 +1,19 @@
 module.exports = function(req, res, next) {
-  var userName = req.body.user_name || 'slackbot';
+    var userName = req.body.user_name || 'slackbot';
 
-  var ticket = req.body.text.match(/jiraticket\(.*?\)/g);
-  var fin = "Can't read ticketnumber, please write like this: 'jiraticket(1234)'";
+    var ticket = req.body.text.match(/jiraticket\(.*?\)/g);
+    var fin = "Can't read ticketnumber, please write like this: 'jiraticket(1234)'";
 
-  if(ticket && ticket[0] ) {
-      var ticketnum = ticket[0].substring(11, ticketnum.length-1);
-      fin = parseInt(fin) || fin;
-  }
-  var botPayload = isInt(fin) ? { text: 'Ticketnumber: ' + fin+1  } : { text: fin};
+    if(ticket && ticket[0] ) {
+        var ticketnum = ticket[0].substring(11, ticketnum.length-1);
+        fin = parseInt(fin) || fin;
+    }
+    var botPayload = isNumber(fin) ? { text: 'Ticketnumber: ' + fin+1  } : { text: fin};
 
-  //avoid infinite loop
-  if(userName !== 'slackbot') { return res.status(200).json(botPayload); }
-  else { return res.status(200).end(); }
+    //avoid infinite loop
+    if(userName !== 'slackbot') { return res.status(200).json(botPayload); }
+    else { return res.status(200).end(); }
+
+    function isNumber(obj) { return !isNaN(parseFloat(obj)); }
 };
+
