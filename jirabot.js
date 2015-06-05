cd54a8a8b4a42eda3666240191acb84eb5db62bc
Maria Heblik <maria@bsolut.com>
2015-06-05 16:16:11 +0200

    regex fix3

--- jirabot.js
+++ jirabot.js
@@ -4,16 +4,17 @@ module.exports = function(req, res, next) {
   var txtcompl = req.body.text;
   var regex = /jiraticket\(.*?\)/g;
   var resArr = txtcompl.match(regex);
-  var res = resArr[0];
+  var ticket = resArr[0];
 
   var fin = "Can't read ticketnumber, please write like this: 'jiraticket(1234)'";
-  console.log(res);
-  if(res) {
-      fin = res.substring(11, res.length-1);
+  console.log(ticket);
+  if(ticket) {
+      console.log("yay");
+      fin = ticket.substring(11, ticket.length-1);
   }
 
   var botPayload = {
-    text: 'Ticketnumber, 666! \n'+ res + '\n ' + fin
+    text: 'Ticketnumber, 666! \n'+ ticket + '\n ' + fin
   };
 
   //avoid infinite loop
