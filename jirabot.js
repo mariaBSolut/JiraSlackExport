8f24beb73dbc9ab5efe1b37a95665b601f68358f
Maria Heblik <maria@bsolut.com>
2015-06-05 14:56:14 +0200

    syntax fix

--- jirabot.js
+++ jirabot.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
   var txt = req.body.text;
   var userName = req.body.user_name || 'slackbot';
   var botPayload = {
-    text: 'Ticketnumber, 666!'+ txt;
+    text: 'Ticketnumber, 666!'+ txt
   };
 
   //avoid infinite loop
