a4ea84c47cbfd62b3f8f05246b1e18a9393b67f9
Maria Heblik <maria@bsolut.com>
2015-06-05 16:26:18 +0200

    ticketnumber fix 1

--- jirabot.js
+++ jirabot.js
@@ -5,10 +5,10 @@ module.exports = function(req, res, next) {
   var fin = "Can't read ticketnumber, please write like this: 'jiraticket(1234)'";
 
   if(ticket && ticket[0] ) {
-      var ticketnum = ticket[0];
-      fin = ticketnum.substring(11, ticketnum.length-1);
+      var ticketnum = ticket[0].substring(11, ticketnum.length-1);
+      fin = parseInt(fin) || fin;
   }
-  var botPayload = { text: 'Ticketnumber: ' + parseInt(fin)+1 };
+  var botPayload = { text: 'Ticketnumber: ' + fin+1  };
 
   //avoid infinite loop
   if(userName !== 'slackbot') { return res.status(200).json(botPayload); }
