e898e3b6bc217383ab5b5d229a8027bb8f51fc6c
Maria Heblik <maria@bsolut.com>
2015-06-08 10:06:50 +0200

    wip getticket

--- jirabot.js
+++ jirabot.js
@@ -6,6 +6,10 @@ module.exports = function(req, res, next) {
 
     if(ticket && ticket[0] ) {
         var ticketnum = ticket[0].substring(11, ticket[0].length-1);
+
+        console.log("BUH");
+        console.log(ticketnum);
+        
         fin = parseInt(fin) || fin;
     }
     var botPayload = isNumber(fin) ? { text: 'Ticketnumber: ' + fin+1  } : { text: fin};
@@ -14,6 +18,6 @@ module.exports = function(req, res, next) {
     if(userName !== 'slackbot') { return res.status(200).json(botPayload); }
     else { return res.status(200).end(); }
 
-    function isNumber(obj) { return !isNaN(parseFloat(obj)); }
+    function isNumber(obj) { return !isNaN(parseFloat(obj)) && isFinite(obj); }
 };
 
